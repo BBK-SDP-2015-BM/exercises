c329e505e610ab56aaf4a7647371132a2c471ab7
BasilMason <basilmason@hotmail.com>
2016-01-23 12:24:33 +0000

    Adapter Pattern

---
+++ DesignPatterns/src/com/sdp/adapter/Xpay.java
@@ -0,0 +1,19 @@
+package com.sdp.adapter;
+
+/**
+ * Created by Basil on 23/01/2016.
+ */
+public interface Xpay {
+    String getCreditCardNo();
+    void setCreditCardNo(String creditCardNo);
+    String getCustomerName();
+    void setCustomerName(String customerName);
+    String getCardExpMonth();
+    void setCardExpMonth(String cardExpMonth);
+    String getCardExpYear();
+    void setCardExpYear(String cardExpYear);
+    Short getCardCVVNo();
+    void setCardCVVNo(Short cardCVVNo);
+    Double getAmount();
+    void setAmount(Double amount);
+}
