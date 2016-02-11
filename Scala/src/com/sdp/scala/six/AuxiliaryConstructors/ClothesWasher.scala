e8cf8e351ed7f762fb65e353c75c93d85581cbce
Basil <basilmason@hotmail.com>
2016-02-11 19:34:32 +0000

    Aux Constructors ex

---
+++ Scala/src/com/sdp/scala/six/AuxiliaryConstructors/ClothesWasher.scala
@@ -0,0 +1,16 @@
+package com.sdp.scala.six.AuxiliaryConstructors
+
+/**
+ * Created by bmason06 on 11/02/2016.
+ */
+class ClothesWasher {
+
+  def this(modelName: String){
+    this()
+  }
+
+  def this(capacity: Double){
+    this()
+  }
+
+}
---
+++ Scala/src/com/sdp/scala/six/AuxiliaryConstructors/ClothesWasher2.scala
@@ -0,0 +1,8 @@
+package com.sdp.scala.six.AuxiliaryConstructors
+
+/**
+ * Created by bmason06 on 11/02/2016.
+ */
+class ClothesWasher2(modelName: String = "", capacity: Double = 0.00) {
+
+}
